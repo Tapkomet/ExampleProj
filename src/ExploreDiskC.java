a6261bb872f306567f4258cd8acf962c6fe10ea3
TrGtr <mrnorie@gmail.com>
2015-02-15 21:18:04 +0200

    Update ExploreDiskC.java

--- src/ExploreDiskC.java
+++ src/ExploreDiskC.java
@@ -4,8 +4,7 @@ import java.io.File;
 public class ExploreDiskC {
 
 	public static void main(String[] args) {
-		File folder = new File(
-				"C:/");
+		File folder = new File("C:/");
 		File[] listOfFiles = folder.listFiles();
 
 		for (File file : listOfFiles) {
