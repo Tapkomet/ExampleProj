50a6a07f8b2344363070796243df374533590c32
TrGtr <mrnorie@gmail.com>
2015-02-15 21:16:27 +0200

    ExploreProj
    Just another example project

---
+++ src/ExploreDiskC.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+
+public class ExploreDiskC {
+
+	public static void main(String[] args) {
+		File folder = new File(
+				"C:/");
+		File[] listOfFiles = folder.listFiles();
+
+		for (File file : listOfFiles) {
+			System.out.println(file.getName());
+		}
+
+	}
+
+}
